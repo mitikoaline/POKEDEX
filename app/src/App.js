166b5a30a81027eeed9acfb140927a1499928659
Vinícius Ribeiro <engvgr@gmail.com>
2021-10-09 13:01:56 -0300

    Modificacoes no App.js para deixar a base de mudança de pagina

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,37 @@
+import { useState } from 'react';
 import './App.css';
+import UserContext from './contexts/User_context';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Home from './pages/Home'
+
 
 function App() {
+
+  const [user,setUser] = useState(null)
+
   return (
-    <div>
-      
-    </div>
+    //Permite o uso do user em todas as paginas. ##Tem que criar uma constante na pagina que quiser usar##
+    <UserContext.Provider value={{user, setUser}}>
+      <Router>
+        <Switch>
+          <Route path="/Login">
+            <Login/>
+          </Route>
+          <Route path="/Register">
+            <Register/>
+          </Route>
+          <Route path="/">
+            <Home/>
+          </Route>
+        </Switch>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
