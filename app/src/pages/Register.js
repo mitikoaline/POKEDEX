a78b4bb80f24500d71f524cb23f8b5127ebcff6b
Vinícius Ribeiro <engvgr@gmail.com>
2021-10-10 21:06:27 -0300

    Basicamente copiei o codigo da funçao Login e colei na funçao Register (com mudança no texto escrito na pagina).

--- app/src/pages/Register.js
+++ app/src/pages/Register.js
@@ -1,7 +1,30 @@
+import { useContext, useState } from "react"
+import { Link } from "react-router-dom"
+import UserContext from "../contexts/User_context"
+
 function Register() {
+    //Permite o uso do 'user' em todas as páginas
+    const {user, setUser} = useContext(UserContext)
+    const  [name, setName] = useState('')
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        //Não é feito o setUser direto no onChage, para ele mudar de página só depois de clicar no botão
+        setUser(name)
+    }
+
     return (
         <div>
-            <h1>Register</h1>
+            <h1>Registre-se</h1>
+            <form onSubmit={handleSubmit}>
+                <label>Nome do treinador:</label>
+                <br/>
+                <input value={name} onChange={(event)=>{setName(event.target.value)}}></input>
+                <br/>
+                <button></button>
+            </form>
+            <br/>
+            <Link to="/Login">Já é um treinador?</Link>
         </div>
     )
 }
