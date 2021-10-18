e1790d1e8fc3494cce7121a6d01cfd38bbb9a3fd
mitikoaline <mitikoarine@gmail.com>
2021-10-18 11:18:13 -0300

    Mudando estilo dos cards

--- app/src/components/Card.js
+++ app/src/components/Card.js
@@ -4,17 +4,23 @@ const Button = styled.button`
   cursor: pointer;
   font-family: 'Acme', sans-serif;
   // background: radial-gradient(circle, rgba(255,255,255,1) 0%, ${(props) => props.color} 100%);
-  // background: ${(props) => props.color};
-  background: radial-gradient(circle, rgba(255,255,255,1) 0%, ${(props) => props.color} 58%, ${(props) => props.color} 59%);
+  background: ${(props) => props.color};
+  // background: radial-gradient(circle, rgba(255,255,255,1) 0%, ${(props) => props.color} 58%, ${(props) => props.color} 59%);
   margin-top: 10px;
   margin-left: 10px;
-  border-radius: 20px;
+  // border-radius: 20px;
   width: 300px;
   padding-top: 10px;
   padding-bottom: 10px;
-  border: none;
+  // border: none;
+  border: 2px solid black;
+
+  h1 {
+    margin-top: -10px;
+  }
 
   p {
+    // margin-top: -10px;
     font-size: 24px;
   }
 
@@ -33,6 +39,7 @@ function Card({name, image, number, kind}) {
   const splitKind = kind.split(";")
 
   var listColors = {
+    dark: "#705848",
     bug: "#7ED578",
     electric: "#FFF34B",
     fairy: "#FF7EE5",
@@ -53,15 +60,17 @@ function Card({name, image, number, kind}) {
   }
 
     return (
-      <Button className="card" color={listColors[splitKind[0]]}>
-        <style>@import url('https://fonts.googleapis.com/css2?family=Acme&display=swap');</style>
-        <img src={image}/>
-        <h1>{name}</h1>
-        {/* {splitKind.map((element)=>
-            <p>{element}</p>
-        )} */}
-        <p>#{number}</p>
-      </Button>
+      <div>
+        <Button className="card" color={listColors[splitKind[0]]}>
+          <style>@import url('https://fonts.googleapis.com/css2?family=Acme&display=swap');</style>
+          <img src={image}/>
+          <h1>{name}</h1>
+          {/* {splitKind.map((element)=>
+              <p>{element}</p>
+          )} */}
+          <p>#{number}</p>
+        </Button>
+      </div>
     );
 }
 
