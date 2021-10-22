b5a7c1a6bc4d677e815395935b50a582feb32d5d
jacckk7 <brenocolima@gmail.com>
2021-10-22 16:38:26 -0300

    feat: Terminei a página que mostra informações Feito o html da página que mostra as informações dos pokémons faltando apenas o CSS.

--- app/src/pages/Pokemon.js
+++ app/src/pages/Pokemon.js
@@ -1,11 +1,32 @@
 import { useState, useEffect } from "react";
 import api from "../resources/api";
-import {useParams} from "react-router-dom"
+import {useParams, Link} from "react-router-dom";
 
 function Pokemon() {
 
-    const [lista, setLista] = useState({})
+    const [lista, setLista] = useState([])
     const {name} = useParams()
+    const [splitKind, setSplitKind] = useState([])
+
+    var listColors = {
+        bug: "#7ED578",
+        electric: "#FFF34B",
+        fairy: "#FF7EE5",
+        fighting: "#F17373",
+        fire: "#FFB433",
+        flying: "#D7F1E9",
+        ghost: "#E2E2E2",
+        grass: "#5EFF53",
+        ground: "#AA8546",
+        ice: "#AEE3FB",
+        normal: "#D7DBA8",
+        poison: "#CE52F9",
+        psychic: "#FFC157",
+        rock: "#757575",
+        steel: "#A1A1A1",
+        water: "#7192FF",
+        dragon: "#43372D",
+    }
 
     useEffect(()=>{
         async function getPokemonData(){
@@ -13,7 +34,14 @@ function Pokemon() {
             api.get(`/pokemons/${name}`)
             .then((resp)=>{
                 setLista(resp.data)
-                console.log(resp.data)
+            })
+            .catch((err)=>{
+                console.log(err)
+            })
+
+            api.get(`/pokemons/${name}`)
+            .then((resp)=>{
+                setSplitKind((resp.data.kind).split(";"))
             })
             .catch((err)=>{
                 console.log(err)
@@ -23,9 +51,23 @@ function Pokemon() {
     }, []
     )
 
+    console.log(splitKind)
+
     return (
-        <div>
+        <div color={listColors[splitKind[0]]}>
+            <Link to="/">Voltar</Link>
             <h1>{lista?.name}</h1>
+            <img src={lista?.image_url}/>
+            <h2>Número na pokédex #{lista.number}</h2>
+            <p>Peso: {(lista.weight)/10} kg</p>
+            <p>Altura: {(lista.height)/10} m</p>
+            <div>
+                <p>{splitKind[0]}</p>
+            </div>
+            <div>
+                <p>{splitKind[1]}</p>
+            </div>
+
         </div>
         
     )
